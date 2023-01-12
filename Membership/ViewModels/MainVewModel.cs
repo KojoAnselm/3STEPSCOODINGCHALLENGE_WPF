0db3897d9978287b88033e901b81c4ccb40ac9a3
antierenyin@acsint.io <antierenyin@acsint.io>
2023-01-12 17:58:28 +0000

    added Validations on Buttons

--- Membership/ViewModels/MainVewModel.cs
+++ Membership/ViewModels/MainVewModel.cs
@@ -1,35 +1,146 @@
 ﻿using Membership.Data;
 using Membership.Model;
 using Membership.Views;
+using Prism.Commands;
+using Prism.Mvvm;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System;
+using System.Windows.Documents;
 
 namespace Membership.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
         private readonly IMemberRepository _memberRepository;
-        public static int memberId = 0;
+        public static int memberId;
+
+        public DelegateCommand _PreviousCommand;
+        public DelegateCommand PreviousCommand
+        {
+            get { return _PreviousCommand; }
+            set { SetProperty(ref _PreviousCommand, value); }
+        }
+        public DelegateCommand NextCommand { get; set;  }
+        public DelegateCommand CloseCommand { get; set; }
 
         public MainViewModel()
         {
             _memberRepository = new MemberRepository();
+            InitCommands();
             InitData();
+        }
+
+        private void InitCommands()
+        {
+            PreviousCommand = new DelegateCommand(GetPreviousRecord,CanGotoPrevious);
+            NextCommand = new DelegateCommand(GetNextRecord,CanGotoNext);
+            CloseCommand = new DelegateCommand(OpenMainWindow);
+        }
+
+
+        #region Command Methods
+
+        private bool CanGotoPrevious()
+        {
+            var MaxRecId = Members?.OrderBy(c => c.Id)?.LastOrDefault()?.Id;
+            var MinRecId = Members?.OrderBy(c => c.Id)?.FirstOrDefault()?.Id;
+
+            if (SelectedActiveMember.Id == MinRecId)
+            {
+                return false;
+            }
+            else if (SelectedActiveMember.Id > MinRecId)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool CanGotoNext()
+        {
+            var MinRecId = Members?.OrderBy(c => c.Id)?.FirstOrDefault()?.Id;
+            var MaxRecId = Members?.OrderBy(c => c.Id)?.LastOrDefault()?.Id;
+            if (SelectedActiveMember.Id == MaxRecId)
+            {
+                return false;
+            }
+            else if (SelectedActiveMember.Id >= MinRecId)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void OpenMainWindow()
+        {
+            memberId = 0;
+            MainWindow.appViewModel.ActiveWindow = new MemberList();
+        }
+
+        private  void GetNextRecord()
+        {
+            if (_selectedActiveMember != null && _selectedActiveMember.Id > 0)
+            {
+                var results = Members.SkipWhile(x => x.Id != SelectedActiveMember.Id).Skip(1).FirstOrDefault();
+                if (results is not null || results?.Id > 0)
+                    SelectedActiveMember = results;
+                else
+                    SelectedActiveMember = Members.First();
+            }
+
+            //To enable Previous and Next Button Visibility at each stage
+            NavigationButtonEnablingCheck();
+        }
+
+        private void NavigationButtonEnablingCheck()
+        {
+            PreviousCommand = new DelegateCommand(GetPreviousRecord, CanGotoPrevious);
+            IsNextEnabled =  CanGotoNext();
+        }
+
+        private void GetPreviousRecord()
+        {
+            if (_selectedActiveMember != null && _selectedActiveMember.Id > 0)
+            {
+                var results = Members.TakeWhile(x => !x.Equals(SelectedActiveMember)).Last(); ;
+                if (results is not null || results?.Id > 0)
+                    SelectedActiveMember = results;
+                else
+                    SelectedActiveMember = Members.Last();
+
+            }
+            //To enable Previous and Next Button Visibility at each stage
+            NavigationButtonEnablingCheck();
+        }
+
+        private void OpenMemberDetails()
+        {
+            if (SelectedMember != null && SelectedMember.Id > 0)
+            {
+                MainViewModel.memberId = SelectedMember.Id;
+                MainWindow.appViewModel.ActiveWindow = new MemberDetails();
+            }
         }
 
         private void InitData()
         {
-            _members = new();
-            Members = new();
-            _selectedMember = new();
-            SelectedMember = new();
             Members = _memberRepository.GetAllMembers();
-            if(memberId  > 0)
+            if (memberId > 0)
             {
                 SelectedActiveMember = Members.FirstOrDefault(c => c.Id == memberId) ?? new();
             }
         }
 
+        #endregion
+
+        #region Properties
         private List<Member> _members;
         public List<Member> Members
         {
@@ -44,22 +155,30 @@ namespace Membership.ViewModels
             set { SetProperty(ref _selectedMember, value); OpenMemberDetails(); }
         }
 
-        private Member _selectedActiveMember;
+        private  Member _selectedActiveMember;
         public Member SelectedActiveMember
         {
-            get { return _selectedMember; }
-            set { SetProperty(ref _selectedMember, value); }
+            get { return _selectedActiveMember; }
+            set { SetProperty(ref _selectedActiveMember, value); }
         }
 
-
-        private void OpenMemberDetails()
+        private bool _isNextEnabled = true;
+        public bool IsNextEnabled
         {
-            if (SelectedMember != null && SelectedMember.Id > 0)
-            {
-                MainViewModel.memberId = SelectedMember.Id;
-                MainWindow.appViewModel.ActiveWindow = new MemberDetails();
-            }
+            get { return _isNextEnabled; }
+            set { SetProperty(ref _isNextEnabled, value); }
         }
+
+
+
+        private bool prevEnabled = true;
+        public bool PrevEnabled {
+            get { return prevEnabled; }
+            set { SetProperty(ref prevEnabled, value); }
+        }
+
+        #endregion
+
 
     }
 }
